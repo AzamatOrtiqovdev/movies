a6c2808651e029a093792c2e4384e3abc4b767dc
AzamatOrtiqovdev <ortiqovazamat890@gmail.com>
2023-05-04 13:29:44 +0500

    add a timer to  js code

--- js/script.js
+++ js/script.js
@@ -46,4 +46,51 @@ window.addEventListener("DOMContentLoaded", () => {
                   })
             }
       })
+
+      // timer
+      const deadline = '2023-06-04'
+
+      function getTimeRemaining(endtime) {
+            const timer = Date.parse(endtime) - Date.parse(new Date()),
+                  days = Math.floor(timer / (24*60*60*1000)),
+                  hours = Math.floor((timer / (60*60*1000))%24),
+                  minutes = Math.floor((timer / (60*1000))%60),
+                  seconds = Math.floor((timer / 1000)%60)
+
+            return {timer, days, hours, minutes, seconds}
+      }
+
+      function getZero(num) {
+            if(num >=0 && num < 10) {
+                  return `0${num}`
+            }else {
+                  return num
+            }
+      }
+
+      function setClock(selector, endtime) {
+            const timerElement = document.querySelector(selector),
+                  days = timerElement.querySelector("#days"),
+                  hours = timerElement.querySelector("#hours"),
+                  minutes = timerElement.querySelector("#minutes"),
+                  seconds = timerElement.querySelector("#seconds")
+                  timeInterval = setInterval(updateClock, 1000)
+
+            updateClock() 
+
+            function updateClock() {
+                  const t = getTimeRemaining(endtime)
+
+                  days.innerHTML = getZero(t.days)
+                  hours.innerHTML = getZero(t.hours)
+                  minutes.innerHTML = getZero(t.minutes)
+                  seconds.innerHTML = getZero(t.seconds)
+
+                  if(t.timer <= 0) {
+                        clearInterval(timeInterval)
+                  }
+            }
+      }
+
+      setClock(".timer", deadline)
 })
